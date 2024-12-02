d08054410b4881aef19dec79b2236ac47b27f6c3
Griffin <omondigriffin4@gmail.com>
2024-12-02 15:16:00 +0300

    creates views and urls for different categories

--- farmlog/views.py
+++ farmlog/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello world!")
+
+def register(request):
+    return HttpResponse("I have registered to use the platform")
+
+def login(request):
+    return HttpResponse("I have logged in to the platform")
+
+def crops(request):
+    return HttpResponse("These are the crops that have been listed on the platform")
+
