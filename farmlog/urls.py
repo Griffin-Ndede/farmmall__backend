a10265cb071a19a56899b1539c221eac217c5003
Griffin <omondigriffin4@gmail.com>
2024-12-02 15:31:30 +0300

    converts the views to class based views

--- farmlog/urls.py
+++ farmlog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('crops', views.crops, name='crops')
+    path('', views.Home.as_view(), name='home'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('crops/', views.Crops.as_view(), name='crops')
 ]
--- farmlog/views.py
+++ farmlog/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello world!")
 
-def register(request):
-    return HttpResponse("I have registered to use the platform")
+class Home(View):
+    def get(self, request):
+        return HttpResponse("Hello world!")
 
-def login(request):
-    return HttpResponse("I have logged in to the platform")
+class Register(View):
+    def get(self, request):
+        return HttpResponse("I have registered to use this platform finally")
+    
+class Login(View):
+    def get(self, request):
+        return HttpResponse("I have logged in to the platform and I can now use it")
+    
+class Crops(View):
+    def get(self, request):
+        return HttpResponse("These are the crops that have been listed on the platform")
 
-def crops(request):
-    return HttpResponse("These are the crops that have been listed on the platform")
 
